efff0f333e8d730a687c74e99b1598d1a2d5bbdf
Pedro Lucas <pedrolucastavaresdasilva@gmail.com>
2020-06-06 21:07:23 -0400

    Principal inicialmente feita

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.principal')
 
 @section('title', 'Bem vindo ao AirCnC')
 
 @section('content')
-<div class="title m-b-md">
-    Página inicial
+<div class="container">
+    <div class="content">
+        <p>
+            Ofereça <strong>spots</strong> para programadores e encontre
+            <strong>talentos</strong> para sua empresa
+        </p>
+        <form>
+            <label for="email">E-MAIL *</label>
+            <input type="email" id="email" placeholder="Seu melhor e-mail" value="email" />
+            <button class="btn" type="submit">
+                Entrar
+            </button>
+        </form>
+    </div>
 </div>
 @endsection
